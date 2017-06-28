2301809a0abed924667e0ae61d770c35507e1e23
Owner <Owner>
2017-06-28 16:11:18 +0800

    add dependencies and open-window

--- dist/index.js
+++ dist/index.js
@@ -1,40 +1,55 @@
 var recorder = require("screen-capture-recorder");
-var scene = new recorder({ x:0, y:0, w:640, h:480 });
-var robot = require("kbm-robot");
+var robot = require("robotjs");
+var opn = require('opn');
 
-scene.warmup(function(err) {
-  //recorder is ready, now start capture
-  scene.StartRecord(function(err) {
-    if(err)
-      console.log("Something got wrong");
-    //capture start _very_ quicky (60ms)
-  });
+//setup of parameters
+const scene = new recorder({ x:0, y:0, w:640, h:480 });
+const screenSize = robot.getScreenSize();
+const width = screenSize.width;
+const height = screenSize.height;
+robot.setMouseDelay(2);
+robot.setKeyboardDelay(20);
 
-  robot.startJar();
+function Callback (err, stage) {
+  if (err) console.error('Download error!', err)
+  else console.log('finished ', stage)
+}
 
-  robot.press("CTRL")
-    .type("ESC", 1000)
-    .release("CTRL")
-    .sleep(1000)
-    .typeString("chrome")
-    .press("ENTER")
-    .release("ENTER")
-    .mouseMOVE(400,300)
-    .go()
-    .then(robot.stopJar);
 
-  setTimeout(function() {
+function tutorialStart(chrome) {
+  return new Promise(function(resolve, reject) {
+    
+    opn('qlear.build', {app: 'chrome'});
 
-    scene.once(recorder.EVENT_DONE, function(err, tmp_path) {
+    if() resolve();
+  })
+}
+
+    scene.warmup(function(err) {
+      //recorder is ready, now start capture
+      scene.StartRecord(function(err) {
+        if(err)
+          console.log("Something got wrong");
+        //capture start _very_ quicky (60ms)
+      });
+
+    
+  /*for (var x = 0, y = 0; x < width; x++, y+= (height/width))
+    {
+      robot.moveMouse(x, y);
+
+    }*/
+    
+
+    scene.once(recorder.EVENT_DONE, function(err, screenRecording) {
       if(!err)
-        console.log("Everything is ok, find video in %s.ogg", tmp_path);
-      //tmp_path is a temporary file that will be deleted on process exit, keep it by renaming it
-      require('fs').renameSync(tmp_path, tmp_path + ".ogg");
+        console.log("Everything is ok, find video in %s.ogg", screenRecording);
+      //recording is a temporary file that will be deleted on process exit, keep it by renaming it
+      require('fs').renameSync(screenRecording, screenRecording + ".ogg");
     });
 
     scene.StopRecord(function(err) {
       if(err)
         console.log("Something got wrong");
     });
-  }, 1000 * 10);
-});
+}, 1000 * 10);
---
+++ dist/robot-test.js
@@ -0,0 +1,51 @@
+//run with node
+
+var robot = require("robotjs");
+
+const screenSize = robot.getScreenSize();
+const width = screenSize.width;
+const height = screenSize.height;
+
+console.log(`screen height is: ` + height + `px`);
+console.log(`screen width is: ` + width + `px`);
+
+robot.setMouseDelay(2);
+//ms per mouse movement event (default is 10)
+
+    var start = setupQlear();
+
+    start.next();
+    setTimeout(function () {
+      start.next();
+    }, 3000);
+    setTimeout(function () {
+      start.next();
+    }, 5000);
+    start.next();
+    setTimeout(function () {
+      start.next();
+    }, 5000);
+
+    function* setupQlear (callback) {
+   console.log('step 1, get open windows menu');
+  robot.keyToggle('control', 'down');
+  robot.keyTap('escape');
+  robot.keyToggle('control', 'up');
+  yield "step 1 complete";
+   console.log('step 2, get chrome');
+  robot.typeString('chrome');
+  robot.keyTap('enter');
+  yield "step 2 complete";
+   console.log('step 3, maximize window');
+  robot.keyToggle('alt', 'down');
+  robot.keyTap('space');
+  yield;
+  robot.keyTap('x');
+  robot.keyToggle('alt', 'up');
+  yield "step 3 complete";
+   console.log("step 4, get qlear")
+  robot.typeString('qlear.build');
+  robot.keyTap('enter');
+  return "qlear ready";/**/
+}
+
